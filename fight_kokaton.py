d8bbd4790ec83692bba3a1ae24dc9baead7bca86
yonezawa riku <c0c25084@edu.teu.ac.jp>
2026-04-28 15:18:55 +0900

    練習４完了

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -161,6 +161,9 @@ def main():
             if bird.rct.colliderect(bomb.rct):
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
                 bird.change_img(6, screen)
+                fonto = pg.font.Font(None, 80)
+                txt = fonto.render("Game Over", True, (255, 0, 0))
+                screen.blit(txt, [WIDTH//2-150, HEIGHT//2])
                 pg.display.update()
                 time.sleep(1)
                 return
@@ -191,3 +194,4 @@ if __name__ == "__main__":
     main()
     pg.quit()
     sys.exit()
+
